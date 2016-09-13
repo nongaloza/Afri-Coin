3276ac2f41a2ece67172f39d3457d745c68ad0ff
Keenan Olsen <keenanolsen28@gmail.com>
2016-09-13 14:40:34 +0000

    added modified devise for stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
-  before_filter :configure_permitted_paramater, if: :devise_controller?
+
+  before_filter :configure_permitted_paramaters, if: :devise_controller?
 
   protected
   def configure_permitted_paramaters
